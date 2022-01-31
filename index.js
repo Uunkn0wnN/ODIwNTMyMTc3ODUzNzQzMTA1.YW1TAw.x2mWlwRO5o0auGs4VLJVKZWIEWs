c5a0a47c0ef47a08c76d8213c2489a21b67ea466
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-02-01 03:03:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -77,14 +77,14 @@ const starts = async (sesName) => {
 				fs.writeFileSync('./lib/json/dataGc.json', JSON.stringify(dataGc, null, 2))
 			}
             if (isGroup && dataGc[from].antitagall && !message.isAdmin && (message.mentionedJidList.length == message.groupMembers.length || message.mentionedJidList.length-1 == message.groupMembers.length)){
-                Client.reply(from, 'Tagall detected', message)
+                Client.reply(from, 'Tagall Terdeteksi', message)
                 client.groupRemove(from, [sender]).catch(() => Client.reply(from, `Jadikan bot admin agar bisa menggunakan fitur antitagall`, message))
             }
             if (isGroup && dataGc[from].antiviewonce && message.type == 'viewOnceMessage'){
                 var msg = {...message}
                 msg.message = message.message.viewOnceMessage.message
                 msg.message[Object.keys(msg.message)[0]].viewOnce = false
-                Client.reply(from, 'ViewOnce detected!', message)
+                Client.reply(from, 'ViewOnce Terdeteksi!', message)
                 client.forwardMessage(from, msg)
             }
 			if (isGroup && !message.isAdmin && dataGc[from].antilink && /chat\.whatsapp\.com/gi.test(body)){
@@ -92,7 +92,7 @@ const starts = async (sesName) => {
 				dtclink.forEach(async l => {
 					checks = await Client.checkInviteLink(l)
 					if(checks.status == 200){
-						Client.reply(from, `Group link detected!`, message)
+						Client.reply(from, `Group link Terdeteksi!`, message)
 						client.groupRemove(from, [sender]).catch(() => Client.reply(from, `Jadikan bot admin agar bisa menggunakan fitur antilink`, message))
 					}
 				})
